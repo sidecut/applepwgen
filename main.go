960f4f2d086c3aeca82164f267aedcbfc8e37aa8
Jim Raden <james.raden@gmail.com>
2024-11-07 22:51:34 -0500

    Move pw gen to a function; use math/rand/v2

--- main.go
+++ main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
-	"math/rand"
-	"time"
+	"math/rand/v2"
+)
+
+var (
+	vowels     = []rune("aeiou")
+	consonants = []rune("bcdfghjklmnpqrstvwxyz")
 )
 
 func main() {
-	A := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	B := []rune("abcdefghijklmnopqrstuvwxyz")
-	rand.Seed(time.Now().UnixNano())
+	password := generatePassword()
 
-	result := make([]rune, 18)
-	for i := 0; i < 18; i++ {
+	println(string(password))
+}
+
+func generatePassword() []rune {
+	pwd18 := make([]rune, 18)
+	for i := 0; i < len(pwd18); i++ {
+		// if i%7 == 6 {
+		// 	password[i] = '-'
+		// } else
 		if i%2 == 0 {
-			result[i] = A[rand.Intn(len(A))]
+			pwd18[i] = consonants[rand.IntN(len(consonants))]
 		} else {
-			result[i] = B[rand.Intn(len(B))]
+			pwd18[i] = vowels[rand.IntN(len(vowels))]
 		}
 	}
 
-	println(string(result))
+	// Add a number at a random position
+	position := rand.IntN(len(pwd18))
+	pwd18[position] = rune('0' + rand.IntN(10))
+
+	// Add an uppercase letter at a random position
+	position = rand.IntN(len(pwd18))
+	pwd18[position] = rune('A' + rand.IntN(26))
+
+	// Now format with dashes
+	pwd20 := append(append(append(append(pwd18[:6], '-'), pwd18[6:12]...), '-'), pwd18[12:18]...)
+
+	return pwd20
 }

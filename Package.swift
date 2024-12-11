f2e3f1c0f586ea1ab9ea7a4216f1faa5d4817ede
Jim Raden <radenj@ccf.org>
2024-12-10 21:25:46 -0500

    Rename swift executable to applepw

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "applepwgen",
+    name: "applepw",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "applepwgen"),
+            name: "applepw"),
     ]
 )
